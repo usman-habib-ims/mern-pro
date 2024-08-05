27e6a5406b0bd7295dffa511d56a17246e7670c6
Usman Habib <usmanhabib802@gmail.com>
2024-08-05 13:56:02 +0500

    create sign up api route

--- MERN 1/api/controllers/auth.controller.js
+++ MERN 1/api/controllers/auth.controller.js
@@ -4,7 +4,11 @@ import bcryptjs from "bcryptjs";
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username:username, email, password:hashedPassword });
+  const newUser = new User({
+    username: username,
+    email,
+    password: hashedPassword,
+  });
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
